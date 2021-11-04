4733f59cdcdf73f58922294013e5c12582a376d8
Ashley Fellows <ashley.r.fellows@gmail.com>
2021-11-04 17:26:24 -0400

    initial commit

---
+++ GeneralStoreAPI.Data/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public string ProductSKU { get; set; }
+
+        [Required]
+        public int ItemCount { get; set; }
+
+        [Required]
+        public DateTime DateOfTransaction { get; set; }
+    }
+}
